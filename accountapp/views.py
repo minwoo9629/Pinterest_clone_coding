2fbfdf183e4d48d0bf85af5ea7fa88050f8e4159
minwoo9629 <minwoo9629@gmail.com>
2020-12-31 15:29:59 +0900

    code edit

--- accountapp/views.py
+++ accountapp/views.py
@@ -23,10 +23,12 @@ class AccountDetailView(DetailView):
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
