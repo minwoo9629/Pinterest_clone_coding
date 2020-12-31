93bd364f22e58cc311c4385c9e3298debcb096fd
minwoo9629 <minwoo9629@gmail.com>
2020-12-31 15:28:20 +0900

    detail delete View

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('detail/<int:pk>', views.AccountDetailView.as_view(), name="detail"),
+    path('update/<int:pk>', views.AccountUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>', views.AccountDeleteView.as_view(), name="delete"),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from accountapp.forms import AccountUpdateForm
 
 
 def hello(request):
@@ -18,3 +19,14 @@ class AccountDetailView(DetailView):
     model = User
     template_name = 'accountapp/detail.html'
     context_object_name = 'target_user'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello')
+    template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
