1c3dc9cbc0056914f1fb1e82b5f2c1b57d69d694
minwoo9629 <minwoo9629@gmail.com>
2020-12-31 15:05:02 +0900

    bootstrap4 and detailView

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+
 
 def hello(request):
     return render(request,'accountapp/hello.html')
@@ -12,3 +13,8 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello')
     template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    template_name = 'accountapp/detail.html'
+    context_object_name = 'target_user'
