7ff8ae92627fa5d9c85d033e5a30fbf34e7128c9
minwoo9629 <minwoo9629@gmail.com>
2021-01-09 17:44:20 +0900

    mobile 화면 맞추기

--- static/js/magic_grid.js
+++ static/js/magic_grid.js
@@ -277,7 +277,7 @@ MagicGrid.prototype.listen = function listen () {
 let magicGrid = new MagicGrid({
     container: '.container',
     animate: true,
-    gutter: 30,
+    gutter: 20,
     static: true,
     useMin: true
   });
