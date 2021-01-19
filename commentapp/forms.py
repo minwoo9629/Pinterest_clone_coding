4d34bfdcd5ab9ea6f6553a833b14bb9dc3ddfa6a
minwoo9629 <minwoo9629@gmail.com>
2021-01-19 15:27:51 +0900

    좋아요 기능 및 article detail 페이지 수정

--- commentapp/forms.py
+++ commentapp/forms.py
@@ -5,3 +5,11 @@ class CommentCreationForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentCreationForm, self).__init__(*args, **kwargs)
+        self.fields['content'].label = ''
+        self.fields['content'].widget.attrs['rows'] = 1
+        self.fields['content'].widget.attrs['cols'] = 20
+        self.fields['content'].widget.attrs['placeholder'] = '댓글입력'
+        self.fields['content'].widget.attrs['id'] = 'comment_input'
