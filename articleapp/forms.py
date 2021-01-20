72b347f3a47ce7eb857d39050aa144fdb684219b
minwoo9629 <minwoo9629@gmail.com>
2021-01-21 02:42:56 +0900

    create.html ui is being modified

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -3,8 +3,6 @@ from django import forms
 from articleapp.models import Article
 from projectapp.models import Project
 class ArticleCreationForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
-                                                         'style': 'heigth: auto; text-align: left;'}))
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
     class Meta:
         model = Article
@@ -12,7 +10,10 @@ class ArticleCreationForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ArticleCreationForm, self).__init__(*args, **kwargs)
-        self.fields['title'].label = "제목"
-        self.fields['project'].label = "테마"
-        self.fields['content'].label = "내용"
-        self.fields['image'].label = "사진"
+        self.fields['title'].widget.attrs['placeholder'] = "제목 추가"
+        self.fields['title'].widget.attrs['class'] = "input_title"
+
+        self.fields['content'].widget.attrs['placeholder'] = "사진에 대해 설명해 보세요."
+        self.fields['content'].widget.attrs['class'] = "input_content"
+        self.fields['content'].widget.attrs['rows'] = 1
+        self.fields['content'].widget.attrs['columns'] = 40
