905ef36443975680197c95d66e67858b730fe9b8
minwoo9629 <minwoo9629@gmail.com>
2021-01-28 18:16:54 +0900

    user profile update ui edit

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -1,8 +1,21 @@
 from django.forms import ModelForm
 from profileapp.models import Profile
+from django import forms
 
 
 class ProfileCreationForm(ModelForm):
+    image = forms.ImageField(widget=forms.FileInput)
     class Meta:
         model = Profile
-        fields = ['nickname', 'message', 'image']
+        fields = ['image', 'nickname', 'message']
+        labels = {
+            'nickname' : '',
+            'message' : '',
+            'image' : '',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileCreationForm,self).__init__(*args,**kwargs)
+        self.fields['nickname'].widget.attrs.update({'class':'input_nickname form-control', 'placeholder':'닉네임'})
+        self.fields['message'].widget.attrs.update({'class':'input_message form-control', 'placeholder':'자신을 소개해주세요.'})
+        self.fields['image'].widget.attrs.update({'class':'form-control', 'id':'input_myimg','onchange':'upload_img(this)'})
